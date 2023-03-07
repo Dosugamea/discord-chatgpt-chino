8bd9a11ff53b03f0c2d3f84de61be01e8dcabdd1
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2023-03-07 23:39:15 +0900

    fix(util_voice): Uses direct file instead bytes Because of the libraries never support Bytes

--- src/util/voice.py
+++ src/util/voice.py
@@ -1,12 +1,18 @@
-from io import BytesIO
+import asyncio
+import os
 import discord
+from uuid import uuid4
 
 
-def play_audio_bytes(voice_client: discord.VoiceClient, audio_bytes: bytes) -> None:
-    audio_bytes = BytesIO(audio_bytes)
-
+async def play_audio_bytes(
+    voice_client: discord.VoiceClient, audio_bytes: bytes
+) -> None:
+    file_name = str(uuid4()) + ".wav"
+    with open(file_name, "wb") as temp_file:
+        temp_file.write(audio_bytes)
     if voice_client and voice_client.is_connected():
-        audio_source = discord.PCMVolumeTransformer(
-            discord.FFmpegPCMAudio(audio_bytes), volume=0.5
-        )
+        audio_source = discord.FFmpegPCMAudio(file_name)
         voice_client.play(audio_source, after=lambda e: print(f"Finished playing: {e}"))
+    while voice_client.is_playing():
+        await asyncio.sleep(0.1)
+    os.remove(file_name)
